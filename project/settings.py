9b2033e07404f017f97c5206ce24775e75a29c08
Rafael José <rafaeldavidjose.dev@gmail.com>
2025-06-16 02:04:25 +0100

    Refactor styles across multiple CSS files for unified font usage and improved responsiveness; update HTML templates for consistency in design elements and remove inline styles; adjust media root path in settings.py for better file management.

--- project/settings.py
+++ project/settings.py
@@ -111,7 +111,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ========== MEDIA FILES ==========
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/rafaeljose/portfolio/Django/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
